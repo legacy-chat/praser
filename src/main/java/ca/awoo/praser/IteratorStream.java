ca53a151e5c59cb7a6d3aa9b9217820ebbffae14
Meep3692 <meep3692@gmail.com>
2023-10-12 21:15:23 -0400

    Created stream that reads from an iterator

---
+++ src/main/java/ca/awoo/praser/IteratorStream.java
@@ -0,0 +1,34 @@
+package ca.awoo.praser;
+
+import java.util.Iterator;
+
+/**
+ * An {@link InputStreamOf} that reads objects from an {@link Iterator}.
+ * @param <T> the type of object to read
+ */
+public class IteratorStream<T> extends InputStreamOf<T> {
+
+    private Iterator<T> iterator;
+
+    /**
+     * Creates a new {@link IteratorStream}.
+     * @param iterator the iterator to read from
+     */
+    public IteratorStream(Iterator<T> iterator) {
+        this.iterator = iterator;
+    }
+
+    /**
+     * Reads the next object from the underlying iterator.
+     * @return the next object in the iterator
+     */
+    @Override
+    protected T readStream() {
+        if (iterator.hasNext()) {
+            return iterator.next();
+        } else {
+            return null;
+        }
+    }
+    
+}
