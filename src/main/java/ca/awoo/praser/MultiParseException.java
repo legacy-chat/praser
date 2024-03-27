73acabc31d625cea93b0e389942eb15688fa5cc9
Meep3692 <meep3692@gmail.com>
2024-03-27 17:24:00 -0400

    MultiParseException now gives its deepest cause as its official cause

--- src/main/java/ca/awoo/praser/MultiParseException.java
+++ src/main/java/ca/awoo/praser/MultiParseException.java
@@ -6,11 +6,30 @@ public class MultiParseException extends ParseException {
     private final List<ParseException> exceptions;
 
     public MultiParseException(Context<?> context, List<ParseException> exceptions) {
-        super(context, "Multiple parse exceptions");
+        super(context, "Multiple parse exceptions", getDeepestThrowable((ParseException[])exceptions.toArray()));
         this.exceptions = exceptions;
     }
 
     public List<ParseException> getExceptions() {
         return exceptions;
+    }
+
+    private static int getThrowableDepth(Throwable t){
+        int depth = 0;
+        while(t != null){
+            depth++;
+            t = t.getCause();
+        }
+        return depth;
+    }
+
+    private static Throwable getDeepestThrowable(Throwable... causes){
+        Throwable deepest = causes[0];
+        for(Throwable cause : causes){
+            if(getThrowableDepth(cause) > getThrowableDepth(deepest)){
+                deepest = cause;
+            }
+        }
+        return deepest;
     }
 }
