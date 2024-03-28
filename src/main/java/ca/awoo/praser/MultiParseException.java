a59fa93b66cef52511ba150763efa833be087969
Meep3692 <meep3692@gmail.com>
2024-03-28 01:10:08 -0400

    MultiParseException give more info in message

--- src/main/java/ca/awoo/praser/MultiParseException.java
+++ src/main/java/ca/awoo/praser/MultiParseException.java
@@ -6,12 +6,21 @@ public class MultiParseException extends ParseException {
     private final List<ParseException> exceptions;
 
     public MultiParseException(Context<?> context, List<ParseException> exceptions) {
-        super(context, "Multiple parse exceptions", getDeepestThrowable(exceptions));
+        super(context, "Multiple parse exceptions: " + getMessages(exceptions), getDeepestThrowable(exceptions));
         this.exceptions = exceptions;
     }
 
     public List<ParseException> getExceptions() {
         return exceptions;
+    }
+
+    private static String getMessages(List<? extends Throwable> causes){
+        StringBuilder sb = new StringBuilder();
+        for(Throwable cause : causes){
+            sb.append(cause.getMessage());
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     private static int getThrowableDepth(Throwable t){
